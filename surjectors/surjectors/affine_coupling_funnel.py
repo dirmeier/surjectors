0ae779bbd187aecc1c250e78380f20a197ca561b
Simon Dirmeier <simd23@protonmail.com>
2022-11-05 23:09:59 +0100

    Implement affine coupling funnel

--- surjectors/surjectors/affine_coupling_funnel.py
+++ surjectors/surjectors/affine_coupling_funnel.py
@@ -1,24 +1,39 @@
 import chex
+import distrax
+import numpy as np
+from chex import Array
+from distrax import ScalarAffine, MaskedCoupling
 from jax import numpy as jnp
 
 from surjectors.surjectors.surjector import Surjector
 
 
 class AffineCouplingFunnel(Surjector):
-    def __init__(self, n_keep, decoder, transform, encoder, kind="inference_surjection"):
-        super().__init__(n_keep, decoder, encoder, kind)
-        self._transform = transform
+    def __init__(self, n_keep, decoder, conditioner, kind="inference_surjection"):
+        super().__init__(n_keep, decoder, None, kind)
+        self._conditioner = conditioner
 
-    def split_input(self, input):
-        split_proportions = (self.n_keep, input.shape[-1] - self.n_keep)
-        return jnp.split(input, split_proportions, axis=-1)
+    def _mask(self, array):
+        mask = jnp.arange(array.shape[-1]) >= self.n_keep
+        mask = mask.astype(jnp.bool_)
+        return mask
+
+    def _inner_bijector(self, mask):
+        def _bijector_fn(params: Array):
+            shift, log_scale = jnp.split(params, 2, axis=-1)
+            return distrax.ScalarAffine(shift, jnp.exp(log_scale))
+
+        return MaskedCoupling(
+            mask, self._conditioner, _bijector_fn
+        )
 
     def inverse_and_likelihood_contribution(self, y):
-        y_plus, y_minus = self.split_input(y)
-        chex.assert_equal_shape([y_plus, y_minus])
-        z, jac_det = self._transform(y_plus, context=y_minus)
-        lp = self.decoder.log_prob(y_minus, context=z)
+        mask = self._mask(y)
+        faux, jac_det = self._inner_bijector(mask).inverse_and_log_det(y)
+        z = faux[:, :self.n_keep]
+        lp = self.decoder.log_prob(faux[:, self.n_keep:], context=z)
         return z, lp + jac_det
 
     def forward_and_likelihood_contribution(self, z):
         raise NotImplementedError()
+
--- surjectors/surjectors/slice.py
+++ surjectors/surjectors/slice.py
@@ -7,6 +7,10 @@ class Slice(Funnel):
     def __init__(self, n_keep, decoder, encoder=None, kind="inference_surjection"):
         super().__init__(n_keep, decoder, encoder, kind)
 
+    def split_input(self, input):
+        split_proportions = (self.n_keep, input.shape[-1] - self.n_keep)
+        return jnp.split(input, split_proportions, axis=-1)
+
     def inverse_and_likelihood_contribution(self, y):
         z, y_minus = self.split_input(y)
         lc = self.decoder.log_prob(y_minus, context=z)
