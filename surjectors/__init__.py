943fd6b51a5f3a6d5e68828db5b5d80e3016b7c7
Simon Dirmeier <simd23@protonmail.com>
2023-04-12 16:17:48 +0200

    Fix forward and likelihood contribution

--- surjectors/__init__.py
+++ surjectors/__init__.py
@@ -2,7 +2,7 @@
 surjectors: Surjection layers for density estimation with normalizing flows
 """
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 from surjectors.bijectors.lu_linear import LULinear
 from surjectors.bijectors.masked_coupling import MaskedCoupling
