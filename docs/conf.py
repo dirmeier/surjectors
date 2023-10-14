4b64f3e2c3dd63b1ce1ef524b9f93209e04ec3d7
Simon Dirmeier <simd23@protonmail.com>
2023-10-15 01:10:11 +0200

    Fix

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,6 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
     "sphinx_autodoc_typehints",
-    'sphinx_book_theme',
     "sphinx_copybutton",
     "sphinx_math_dollar",
     "IPython.sphinxext.ipython_console_highlighting",
@@ -43,7 +42,7 @@ exclude_patterns = [
     "examples/*py"
 ]
 
-html_theme = "sphinx_book_theme"
+html_theme = "furo"
 
 html_theme_options = {
     "repository_url": "https://github.com/dirmeier/surjectors",
